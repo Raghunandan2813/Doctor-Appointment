032c24b20e481b6e7bb43cfb1597c86e5de49b2c
Raghunandan <raghu9555k@gmail.com>
2025-10-05 21:59:07 +0530

    Day 1: Initial commit - Started the project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header';
+import SpecialityMenu from '../components/SpecialityMenu';
+
+function Home() {
+    return (
+        <div>
+            <Header/>
+            <SpecialityMenu/>
+        </div>
+    );
+}
+
+export default Home;
